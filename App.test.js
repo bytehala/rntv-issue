57b5164effbbbd5da123e47afe06de37335cf130
Lem <ulna-draftee-0z@icloud.com>
2024-09-21 22:38:31 -0600

    Proof of fix 4.0.0

--- App.test.js
+++ App.test.js
@@ -3,14 +3,15 @@ import React from 'react';
 import {render, fireEvent} from '@testing-library/react-native';
 import App from './App';
 
-test('Should present the route title on scene content', () => {
-  const {toJSON, getByLabelText, getByText} = render(<App />);
+test('Should present the route title on scene content', async () => {
+  const {toJSON, getByLabelText, getByText, queryByText} = render(<App />);
 
   expect(toJSON()).toMatchSnapshot(); // only first scene rendered
 
   expect(getByText('First route')).toBeDefined();
 
-  fireEvent.press(getByLabelText('Second'));
+  fireEvent.press(getByLabelText('Second'), {});
 
   expect(getByText('Second route')).toBeDefined();
+  expect(await queryByText('First route')).toBeNull();
 });
